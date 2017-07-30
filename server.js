0851b543bb6dbaaee1b90e547193dbcfec89fc32
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-30 10:52:42 -0400

    Could not get li's to function for tasks completed

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+
+const mustacheExpress = require("mustache-express")
+const bodyParser = require("body-parser")
+const expressValidator = require("express-validator")
+
+app.engine("mst", mustacheExpress())
+app.set("view engine", "mst")
+app.set("views", "./views")
+app.use(express.static("public"))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(expressValidator())
+
+const todos = ["Wash the car", "Do the dishes", "Clean the kitchen"]
+
+const done = ["Sweep the floors", "Clean the toilets", "Dust the furniture"]
+
+app.get("/", (req, res) => {
+  res.render("home", { todos: todos })
+})
+
+app.post("/", (req, res) => {
+  todos.push(req.body.todo)
+  res.redirect("/")
+})
+
+app.listen(3000, () => {
+  console.log("Listening")
+})
