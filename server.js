1cc4581df049887753d4289a5ab6072848eb9e56
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-31 15:40:10 -0400

    Added todos.json file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 const mustacheExpress = require("mustache-express")
 const bodyParser = require("body-parser")
 const expressValidator = require("express-validator")
+const expressSession = require("express-session")
+const jsonfile = require("jsonfile")
 
 app.engine("mst", mustacheExpress())
 app.set("view engine", "mst")
@@ -14,11 +16,20 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(expressValidator())
 
+app.use(expressSession({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
 const todos = ["Wash the car", "Do the dishes", "Clean the kitchen"]
 const done = ["Sweep the floors", "Clean the toilets", "Dust the furniture"]
 
 app.get("/", (req, res) => {
   res.render("home", { todos: todos, done: done })
+  jsonfile.writeFile('todos.json', todos, { spaces: 2 }, err => {
+    console.log(`todos.json error: ${err}`)
+  })
 })
 
 app.post("/addToDo", (req, res) => {
